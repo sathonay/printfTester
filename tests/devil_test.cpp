cca62747013f03cd9ed7364f9e571b08b50bef91
Alexis <33222808+sathonay@users.noreply.github.com>
2024-12-23 00:32:59 +0100

    Update devil_test.cpp

--- tests/devil_test.cpp
+++ tests/devil_test.cpp
@@ -22,15 +22,16 @@ int main(int ac, char ** av)
 	testName = av[0];
 	if (ac != 1)
 		printTestNumber(av[1], TEST_LIMIT);
-	SUBCATEGORY(1, 8, cout << endl << FG_LGRAY << "subcategory: limbo" << RESET_ALL << endl;)
+	SUBCATEGORY(1, 9, cout << endl << FG_LGRAY << "subcategory: limbo" << RESET_ALL << endl;)
 	TEST(1, print(" %11.10u ", 1234));
 	TEST(2, print(" %11.10d ", 1234));
 	TEST(3, print(" %11.10i ", 1234));
-	TEST(4, print(" %#10.0u ", 0));
-	TEST(5, print(" %#10.0d ", 0));
-	TEST(6, print(" %#10.0i ", 0));
-	TEST(7, print(" %#10.0x ", 0));
-	TEST(8, print(" %2.1 3.2 4.3s ", "this one doesn't work on ubuntu but works on osx (ubuntu -> '%2.1 3.2 4.3s' | osx -> works)"));
+	TEST(4, print(" %#11.10x ", 1234));
+	TEST(5, print(" %10.0u ", 0));
+	TEST(6, print(" %10.0d ", 0));
+	TEST(7, print(" %10.0i ", 0));
+	TEST(8, print(" %#10.0x ", 0));
+	TEST(9, print(" %2.1 3.2 4.3s ", "this one doesn't work on ubuntu but works on osx (ubuntu -> '%2.1 3.2 4.3s' | osx -> works)"));
 	cout << ENDL;
 	return (0);
 }
